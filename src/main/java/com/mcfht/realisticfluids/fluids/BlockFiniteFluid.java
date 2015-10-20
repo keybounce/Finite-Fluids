565b6898db84cb6410428eeda266b2be9f8b2806
Keybounce <michael-github@stb04.nccom.com>
2015-10-19 17:15:36 -0700

    And let mod fluids absorb excess water

--- src/main/java/com/mcfht/realisticfluids/fluids/BlockFiniteFluid.java
+++ src/main/java/com/mcfht/realisticfluids/fluids/BlockFiniteFluid.java
@@ -312,7 +312,10 @@ public class BlockFiniteFluid extends BlockDynamicLiquid
     public byte checkFlow(ChunkData data, final int x0, final int y0, final int z0, final int dx, final int dy, final int dz,
             final Block b1, final int m, final int l0)
     {
-        if (b1 == Blocks.air || Util.isSameFluid(this, b1))
+        if (b1 == Blocks.air || 
+                // Util.isSameFluid(this, b1)   // Old: Same finite fluid
+                this.getMaterial() == b1.getMaterial()  // New: Same type of (possibly mod) fluid.
+           )
             return 1;
 
         final int x1 = x0 + dx;
