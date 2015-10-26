3c09b5e1d01dc106f1dfb5e55810f80ead3402cd
Keybounce <michael-github@stb04.nccom.com>
2015-10-25 17:29:56 -0700

    Update mod info

--- src/main/java/com/mcfht/realisticfluids/FluidModInfo.java
+++ src/main/java/com/mcfht/realisticfluids/FluidModInfo.java
@@ -4,7 +4,7 @@ import cpw.mods.fml.common.ModMetadata;
 public class FluidModInfo {
 
     public static final String MODID = "finitewater";
-    public static final String VERSION = "0.3";
+    public static final String VERSION = "0.3.1";
     public static final String AUTHOR = "FHT";
 	
 	public static void get(ModMetadata meta)
@@ -12,9 +12,9 @@ public class FluidModInfo {
 		meta.modId = MODID;
 		meta.name = MODID;
 		meta.version = VERSION;
-		meta.credits = AUTHOR;
+		meta.credits = AUTHOR + ", Keybounce";
 		meta.authorList.add(AUTHOR);
-		meta.description= "";
+		meta.description= "Finite, flowing water, more realistic than forge fluids";
 		meta.url = "";
 		meta.updateUrl= "";
 		meta.screenshots= new String[0];
